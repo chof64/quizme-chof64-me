e1acee08d8eb2957e15d3a640961dcae9e57c9f6
Chad Fernandez <developer@chadfernandez.me>
2024-11-20 08:55:36 +0800

    update and cleanup trpc routers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+
+import { quizRouter } from "./routers/quiz";
+import { triviaRouter } from "./routers/trivia";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,8 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  trivia: triviaRouter,
+  quiz: quizRouter,
 });
 
 // export type definition of API
