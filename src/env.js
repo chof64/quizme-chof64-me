df6e72d353a4ddf627c4a11338106ed8cf5a37d0
Chad Fernandez <developer@chadfernandez.me>
2024-12-27 12:13:38 +0800

    add postgres with trpc integration

--- src/env.js
+++ src/env.js
@@ -7,7 +7,11 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    NODE_ENV: z.enum(["development", "test", "production"]),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+
+    DATABASE_URL: z.string().url(),
   },
 
   /**
@@ -25,7 +29,8 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+
+    DATABASE_URL: process.env.DATABASE_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
