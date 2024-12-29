85e5b0879e409439c8697d84b11593ab8583ba9f
Chad Fernandez <developer@chadfernandez.me>
2024-12-29 08:12:32 +0800

    update profile icon to be a dropdown with more options

--- src/components/header/ProfileNav.tsx
+++ src/components/header/ProfileNav.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import React from "react";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 
 export default function ProfileNav() {
   const { data: session } = useSession();
@@ -14,12 +21,24 @@ export default function ProfileNav() {
 
   return (
     <div className="ml-auto w-fit">
-      <Avatar className="h-8 w-8">
-        <AvatarImage src={session.user.image ?? undefined} />
-        <AvatarFallback>
-          {(session.user.name?.[0] ?? "??").toUpperCase()}
-        </AvatarFallback>
-      </Avatar>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src={session.user.image ?? undefined} />
+            <AvatarFallback>
+              {(session.user.name?.[0] ?? "??").toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-screen max-w-xs shadow" align="end">
+          <DropdownMenuItem disabled>Profile</DropdownMenuItem>
+          <DropdownMenuItem disabled>Settings</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => signOut({ redirectTo: "/" })}>
+            Sign out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
