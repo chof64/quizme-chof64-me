05fdfc488110277bd0cc39e59dfd5a70a22bfd6f
Chad Fernandez <developer@chadfernandez.me>
2024-12-29 15:34:53 +0800

    remove heart functionality and polished the code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default async function LearnPage() {
   const session = await auth();
   if (!session) redirect("/");
 
-  await api.trivia.date.prefetch({
+  await api.trivia.daily.prefetch({
     month: new Date().getMonth() + 1,
     day: new Date().getDate(),
   });
